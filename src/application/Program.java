03f536c63707ae7ca8eb931caa19ff89d3f8f471
BarbaraPSousa <barbarapsousa@hotmail.com>
2021-06-12 10:02:13 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(2, "Books");//inst. class
-		System.out.println(obj);// teste
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);//inst Sellerclass seler com valores
+		System.out.println(seller);// teste
 
 	}
 
