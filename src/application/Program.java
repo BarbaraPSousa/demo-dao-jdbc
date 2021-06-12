2287800a30ac4c5b5f6f59eaafd0d6ca11cc1790
BarbaraPSousa <barbarapsousa@hotmail.com>
2021-06-12 09:43:50 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(2, "Books");//inst. class
+		System.out.println(obj);// teste
+
+	}
+
+}
