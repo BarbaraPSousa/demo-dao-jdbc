441cdbb005269187a583623c7b8ce4ae9f15956d
BarbaraPSousa <barbarapsousa@hotmail.com>
2021-06-12 10:33:28 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,8 +14,9 @@ public class Program {
 		Department obj = new Department(2, "Books");//inst. class
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);//inst Sellerclass seler com valores
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();//instanciando um Sellerdao, n precissa do new.
+		
 		System.out.println(seller);// teste
-
 	}
-
 }
