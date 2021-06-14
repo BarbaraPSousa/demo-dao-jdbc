e7460ecb0bce4a6e4a18cf9bba26672f43510686
BarbaraPSousa <barbarapsousa@hotmail.com>
2021-06-14 08:49:35 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,7 +26,41 @@ public class SellerDaoJDBC implements SellerDao{
 	
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4,obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();//executando
+			
+			if(rowsAffected > 0) {//verificando se inseriu dados
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {//se tiver inserido
+					int id = rs.getInt(1);//id gerado
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);//fechando 
+			}
+			else {
+				throw new DbException("Unexpected error! No rows affected");//se não inserir lanca exceção
+			}
+		}
+		catch (SQLException e) {//tratando possivel exceção
+			throw new DbException(e.getMessage());
+		}
+		finally {//fechando
+			DB.closeStatement(st);
+		}
 		
 	}
 
